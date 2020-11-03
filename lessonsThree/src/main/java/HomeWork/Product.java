a8ca4cb9f97abf36adfc3ca9e4ac77ba27b4fe9a
GuskoVVadia <guskov.vadim@gmail.com>
2020-11-03 09:04:08 +0300

    Домашнее задание - задача первая

--- lessonsThree/src/main/java/HomeWork/Product.java
+++ lessonsThree/src/main/java/HomeWork/Product.java
@@ -1,4 +1,71 @@
 package HomeWork;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "products_tbl")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "products_fld_id")
+    private Long id;
+
+    @Column(name = "product_fld_name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "purchase_tbl",
+            joinColumns = @JoinColumn(name = "product_fld_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_fld_id")
+    )
+    private List<Customer> customers;
+
+    public Product() {
+        this.customers = new ArrayList<>();
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean addCustomer(Customer customer){
+        return this.customers.add(customer);
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
+    }
 }
--- lessonsThree/src/main/java/Main.java
+++ lessonsThree/src/main/java/Main.java
@@ -1,9 +1,11 @@
-package HomeWork;
-
+import HomeWork.Customer;
+import HomeWork.Product;
 import org.hibernate.cfg.Configuration;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,10 +13,63 @@ public class Main {
         EntityManagerFactory factory = new Configuration().configure("hibernate.xml").buildSessionFactory();
         EntityManager manager = factory.createEntityManager();
 
-        manager.getTransaction().begin();
+        Product milk = new Product("milk");
+        createEntity(manager, milk);
+        Product bread = new Product("bread");
+        createEntity(manager, bread);
+        Product ham = new Product("ham");
+        createEntity(manager, ham);
 
-        manager.persist(new Object());
+        Customer happyMilkMan = new Customer("Igor");
+        createEntity(manager, happyMilkMan);
+
+        Customer userTwo = new Customer("Alex");
+        createEntity(manager, userTwo);
+
+        happyMilkMan = readEntity(manager, happyMilkMan.getId(), Customer.class);
+        List<Product> purchaseHappyMilk = new ArrayList<>();
+        Product product = readEntity(manager, 1, Product.class);
+        purchaseHappyMilk.add(product);
+        happyMilkMan.setProducts(purchaseHappyMilk);
+        saveEntity(manager, happyMilkMan);
+
+        List<Customer> customerList = new ArrayList<>();
+        customerList.add(happyMilkMan);
+        product.setCustomers(customerList);
+        saveEntity(manager, product);
+
+
+
+
+
 
 
     }
+    private  static <T> void createEntity(EntityManager em, T entity){
+        em.getTransaction().begin();
+        em.persist(entity);
+        em.getTransaction().commit();
+    }
+
+    private static <T> T readEntity(EntityManager em, long id, Class<T> tClazz){
+        em.getTransaction().begin();
+        T object = em.find(tClazz, id);
+        em.getTransaction().commit();
+        return object;
+    }
+
+    private static <T> T saveEntity(EntityManager em, T entity){
+        em.getTransaction().begin();
+        T savedEntity = em.merge(entity);
+        em.getTransaction().commit();
+        return savedEntity;
+    }
+
+    private static <T> void deleteEntity(EntityManager em, T entity){
+        em.getTransaction().begin();
+        em.detach(entity);
+        em.getTransaction().commit();
+    }
+
+
 }
